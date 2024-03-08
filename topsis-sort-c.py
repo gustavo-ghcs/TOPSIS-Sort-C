810e050fa79bee5a6b53c808eac0d84dfbd0fc0e
Geovanna Domingos <gmdomingosn@gmail.com>
2024-03-07 21:01:07 -0300

    Alteração de comentários e adição de tratamento da base de dados (#1)
    Co-authored-by: Gustavo Hollanda <gustavo-ghcs@users.noreply.github.com>
    Co-authored-by: Giovanna Machado <giovannamachado@users.noreply.github.com>
    Co-authored-by: Higor Cunha <higorcunha1@users.noreply.github.com>
    Co-authored-by: Maria Eduarda Oliveira de Melo <maduMelo@users.noreply.github.com>

--- topsis-sort-c.py
+++ topsis-sort-c.py
@@ -1,8 +1,9 @@
 import numpy as np
-import pandas as pd 
+import csv # Para a leitura dos dados
 
 def normalize(matrix, method='max'):
     # Normaliza a matriz de acordo com o método especificado.
+    # Na teoria do TOPSIS, a normalização é importante para garantir que diferentes critérios, que podem estar em diferentes escalas, tenham o mesmo peso na análise.
     if method == 'max':
         return matrix / matrix.max(axis=0)
     elif method == 'interval':
@@ -12,9 +13,10 @@ def normalize(matrix, method='max'):
     else:
         raise ValueError("Método de normalização inválido. Use 'max' ou 'interval'.")
 
-def euclidean_distance(v1, v2):
+def euclidean_distance(vector1, vector2): 
     # Calcula a distância euclidiana entre dois vetores.
-    return np.sqrt(np.sum((v1 - v2)**2))
+    # Na teoria do TOPSIS, as distâncias entre as alternativas e as soluções ideais (positivas e negativas) são calculadas para determinar a proximidade relativa das alternativas em relação às soluções ideais.
+    return np.sqrt(np.sum((vector1 - vector2)**2))
 
 def topsis_sort_c(decision_matrix, boundary_profiles, weights, normalization_method='max'):
     # Cria uma matriz de decisão completa contendo a matriz original, seus perfis característicos e valores máximo e mínimo de cada coluna.
@@ -46,3 +48,46 @@ def topsis_sort_c(decision_matrix, boundary_profiles, weights, normalization_met
         assignments.append(profile_diffs.index(min(profile_diffs)) + 1) 
 
     return assignments
+
+
+# Base de dados esperada em csv
+data_csv = []
+with open('./data.csv', 'r') as arquivo_csv: # Abrir o arquivo CSV e ler os valores relacionados aos critérios de cada alternativa
+    csv_reader = csv.reader(arquivo_csv)
+    for linha in csv_reader:
+        data_linha = [float(valor) for valor in linha]
+        data_csv.append(data_linha)
+
+# matriz de decisão incial
+matriz_numpy = np.array(data_csv) # Transformar o csv em uma matriz NumPy para facilitar manipulação de dados
+
+# -Pseudocódigo
+# Receber número de alternativas
+# Receber número de critérios
+# Receber o vetor de pesos
+# Receber o método de normalização
+# Chamar o TOPSIS
+
+####
+
+# # Exemplo
+# decision_matrix = np.array([[5, 2, 10],
+#                             [3, 8, 5],
+#                             [9, 4, 1]])
+
+# # Matriz de perfis de fronteira
+# boundary_profiles = np.array([[7, 5, 3]])
+
+# # Vetor de pesos
+# weights = np.array([0.3, 0.5, 0.2])
+
+# # Método de normalização
+# normalization_method = "max"
+
+# # Execução do algoritmo
+# assignments = topsis_sort_c(decision_matrix, boundary_profiles, weights, normalization_method)
+
+# # Impressão dos resultados
+# print(assignments)
+
+# # Saída - [1, 2, 1]
