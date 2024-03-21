688bd703e08414e6d96cd21c521402dcaf7a13f0
gustavo-ghcs <ghcs@cin.ufpe.br>
2024-03-21 01:39:25 -0300

    Subindo lib

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
 
 setup(
     name='TOPSIS-Sort-C',
     version='0.1',
+    license='MIT License',
     description='TOPSIS-Sort-C implemented in python',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author='Gustavo Hollanda, Geovanna Domingos, Giovanna Machado, Higor Cunha, Maria Eduarda Melo',
+    author_email='ghcs@cin.ufpe.br',
     url='https://github.com/gustavo-ghcs/TOPSIS-Sort-C',
-    packages=find_packages(),
     install_requires=[
         'numpy>=1.26.4',
     ],
