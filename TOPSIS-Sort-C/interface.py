d5899937f2964b07b343b5bad7f01f501889783a
gustavo-ghcs <ghcs@cin.ufpe.br>
2024-03-20 21:23:39 -0300

    Adicionando .gitignore

--- TOPSIS-Sort-C/interface.py
+++ TOPSIS-Sort-C/interface.py
@@ -1,5 +1,4 @@
 import streamlit as stlit
-import streamlit as stlit
 from topsis_sort_c import *
 import pandas as pd
 import numpy as np
@@ -28,10 +27,12 @@ if uploaded_file1 is not None and uploaded_file2 is not None and uploaded_file3 
     df_pesos = pd.read_csv(uploaded_file3, header=None)
     
 
-    # Convertendo os dataframes em listas de listas
     matriz = df_matriz.values.tolist()
-    perfis = df_perfis.values.tolist()
-   # pesos = np.array(df_pesos)
+    perfis = [
+    [10, 10, 9],  # Bom
+    [8, 7, 9],  # Médio
+    [7, 6, 6]  # Ruim
+]
     pesos = np.array([9, 8, 8])
 
     topsis(matriz, perfis, pesos)
